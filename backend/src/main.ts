424be971006a58d34f87cf564f3871bfde17ffc0
Momoh Osaze <76972139+ItisCyph3r@users.noreply.github.com>
2026-01-18 00:50:05 +0100

    update: allowed support for multiple cors origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN?.includes(',')
+      ? process.env.CORS_ORIGIN.split(',').map((origin) => origin.trim())
+      : process.env.CORS_ORIGIN || 'http://localhost:3000',
     credentials: true,
   });
 
